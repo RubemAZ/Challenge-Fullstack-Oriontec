cb8dcc178e8bb23859edd5555b9a213555ab476c
Rubem Mazetto <Raz.rubem@gmail.com>
2024-10-07 23:31:19 -0300

    frontend bug fixes

--- crud/frontend/app/customers/page.tsx
+++ crud/frontend/app/customers/page.tsx
@@ -11,24 +11,51 @@ const CustomersPage = () => {
     const customerList = async () => {
       try {
         const data: CustomerData[] = await fetchCustomers() as CustomerData[]
-
+        
         setCustomers(data.map((item) => CustomerPresenter.present(item)))
       } catch (error) {
         console.error(error);
       }
     };
+    
+    {customers.map((customer) => (
+      <li key={customer.id}>{customer.name}</li>
+    ))}
 
+    
     customerList();
   }, []);
   
   return (
     <div>
-      <h1>Lista de Clientes</h1>
-      <ul>
-        {customers.map((customer) => (
-          <li key={customer.id}>{customer.name}</li>
-        ))}
-      </ul>
+      <div>
+        <h1>Lista de Clientes</h1>
+        <table className="min-w-full border-collapse border border-gray-300">
+          <thead>
+            <tr>
+              <th className="border border-gray-300 p-2">Nome</th>
+              <th className="border border-gray-300 p-2">Email</th>
+              <th className="border border-gray-300 p-2">RG/CPF</th>
+              <th className="border border-gray-300 p-2">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((customer) => (
+              <tr key={customer.id}>
+                <td className="border border-gray-300 p-2">{customer.name}</td>
+                <td className="border border-gray-300 p-2">{customer.email}</td>
+                <td className="border border-gray-300 p-2">{customer.document}</td>
+                <td className="border border-gray-300 p-2">
+                  {/* Botões para editar e excluir */}
+                  <button onClick={() => console.log('Edit', customer.id)}>Editar</button>
+                  <button onClick={() => console.log('Delete', customer.id)}>Excluir</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 };
