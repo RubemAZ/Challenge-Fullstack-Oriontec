be858a0b80c8f643350fcb304b2530123b8f7cd1
Rubem Mazetto <Raz.rubem@gmail.com>
2024-10-10 20:55:53 -0300

    Refactoring backend for Vercel

--- crud/backend/api/index.ts
+++ crud/backend/api/index.ts
@@ -1,13 +1,9 @@
-import dotenv from 'dotenv';
-import '../src/main'
-dotenv.config();
 
-const express = require("express");
+import express from 'express';
+import { ExpressHttpServer } from '../src/external/web/express/ExpressHttpServer';
+
 const app = express();
+const server = new ExpressHttpServer(app);
 
-app.get("/", (req, res) => res.send("Express on Vercel running"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
-
+// Função que será exportada e executada pelo Vercel
+export default server.getApp();
