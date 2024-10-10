b77aae5263b01f378deebe7e509330d86cae77c3
Rubem Mazetto <Raz.rubem@gmail.com>
2024-10-10 17:39:16 -0300

    Change Vercel baseURL config and destination root

--- crud/frontend/src/adapters/http/api.ts
+++ crud/frontend/src/adapters/http/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://oriontec-challenge-backend.vercel.app/api',
+    baseURL: 'oriontec-crud-api.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
